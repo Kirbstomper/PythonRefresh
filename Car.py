c8ac0c0bdf9714f4a935e0cb00f8316e47a5e1b0
Kirbstomper <WingedKraby@gmail.com>
2015-12-24 16:17:44 -0500

    Starting the year of Pijects

--- Car.py
+++ Car.py
@@ -1,22 +1,22 @@
 class Car:
     'This class should simulate a car'
 
-    def __init__(self,model,year):
+    def __init__(self, model, year):
         self.model = model
         self.year = year
 
-
     def displayYear(self):
-        print "This car was mmade in %d" % self.year
+        print "This car was made in %d" % self.year
 
 
     def displayCar(self):
-        print "This is a year",self.year,self.model
+        print "This is a year", self.year, self.model
 
-"Now the actual program begins"
 
-nissan = Car("versa",2013)
-ford = Car("mustang",1996)
+#A sample used when the program is run just to test things out.
+
+nissan = Car("versa", 2013)
+ford = Car("mustang", 1996)
 
 nissan.displayYear()
 nissan.displayCar()
