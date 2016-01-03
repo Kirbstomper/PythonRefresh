a591b3f6256a216d4b46c7b21c1d0800b9be273d
Kirbstomper <WingedKraby@gmail.com>
2016-01-03 02:19:57 -0500

    Added a quick GUI for generating to console. Will expand upon later.

--- YearOfCode/NameGenerator/NameGenerator.py
+++ YearOfCode/NameGenerator/NameGenerator.py
@@ -1,4 +1,5 @@
 from random import randint
+
 """Project # 1, The name generator
  This project fist anc foremost is meant to generate a random name each time it is run
  The bonus for this is not only generating a name, but also random credentials such as a...
@@ -7,43 +8,53 @@ from random import randint
    -Christopher Smith, 12/24/2015
 """
 
+
 # So to generate all this stuff I am going to use a txt file full of first names, and randomly select from it
 # Its gonna be the same for last. Cause who wants to actually type a ton of names?
-
-#some code created by Stackoverflow user SilentGhost, I figure, why reinvent the wheel?
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-# Code use ends
-
 def random_name(list):
-    nameList = open(list,'r' ).readlines()
+    nameList = open(list, 'r').readlines()
     listlen = file_len(list)
-    return nameList[randint(0,listlen-1)].split(' ', 1)[0]
+    return nameList[randint(0, listlen - 1)].split(' ', 1)[0]
+# some code created by Stackoverflow user SilentGhost, I figure, why reinvent the wheel?
+
+
+def file_len(fname):
+        with open(fname) as f:
+            for i, l in enumerate(f):
+                pass
+        return i + 1
+
+    # Code use ends
+
+
+
+
 
 def get_first():
     return random_name('first.txt')
 
+
 def get_last():
     return random_name('last.txt')
+
 
 def get_address():
     street = random_name('streets.txt')
     suffix = random_name('suffix.txt')
-    zipcode = (randint(10000,99999))
-    number = (randint(1,9999))
+    zipcode = (randint(10000, 99999))
+    number = (randint(1, 9999))
     state = random_name('states.txt')
     city = random_name('cities.txt')
 
-    return(str(number)+" " +street+" "+suffix+" "+city+" "+state+" "+str(zipcode))#WORK ON FORMATTING #######
+    return (str(number) + " " + street + " " + suffix + " " + city + " " + state + " " + str(
+        zipcode))  # WORK ON FORMATTING #######
+
 
 def get_phone():
+    return randint(1000000, 9999999)
 
-    return randint(1000000,9999999)
-
-print "First Name:",get_first()
-print "Last Name:",get_last()
-print get_address()
-print get_phone()
+def generate():
+    print "First Name:", get_first()
+    print "Last Name:", get_last()
+    print get_address()
+    print get_phone()
